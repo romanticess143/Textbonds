8f03ecfc521bd9ea10bb3d7d215e9666ce5e1b8e
romanticess143 <32557268+romanticess143@users.noreply.github.com>
2017-11-16 19:45:15 -0500

    Add files via upload

---
+++ Textbonds/firebase_config.js
@@ -0,0 +1,11 @@
+
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCrdDzAd8FxGR56Sz15-C-0dITsRxDM-9o",
+    authDomain: "text-bonds.firebaseapp.com",
+    databaseURL: "https://text-bonds.firebaseio.com",
+    projectId: "text-bonds",
+    storageBucket: "text-bonds.appspot.com",
+    messagingSenderId: "1082547738065"
+  };
+  firebase.initializeApp(config);
